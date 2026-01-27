03490260f4e87f89727e3a780884ba192de5d445
Yasmim Namie <yas.nami.ti@gmail.com>
2026-01-27 15:35:18 -0400

    Alteração feita

--- DATA_analysis.py
+++ DATA_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
 from IPython.display import display
 
-
+print("Testando comandos que não devem ser usados para enviar ao repositório remoto.")
+print ("Commits que devem ser enviados ao repositório remoto.")
