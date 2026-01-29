6d29b7ec90412272ccd9ae0dee7fcaff2e008347
Yasmim Namie <yas.nami.ti@gmail.com>
2026-01-29 16:00:20 -0400

    documentos de teste

--- DATA_analysis.py
+++ DATA_analysis.py
@@ -1,5 +1,1 @@
-import pandas as pd
-from IPython.display import display
-
-print("Testando comandos que não devem ser usados para enviar ao repositório remoto.")
-print ("Commits que devem ser enviados ao repositório remoto.")
+print ("Documento para teste")
